98dfb3662ac38ec1c6a4b48badd6d58132aeec36
g.adelizzi <g.adelizzi@b8940b84-930b-f946-a580-441c179c1ffe>
2010-11-03 21:52:12 +0000

    3.11

--- trunk/JaCompiler/testja/Test.java
+++ trunk/JaCompiler/testja/Test.java
@@ -15,14 +15,15 @@ public class Test {
     
     public float m(int k, int j){ return 0;}
     public UFF m(int k, int j){ return 0;}
-    public float m(int k){
+    public void m(int k){
+        Test t = new Test();
 
-        double ip=0;
-        ip++++;
+        int a = 0;
+        int c = t + t[6];
+        int f = c + a;
+
         int[][] i = new int[][]{{},{}};
-        Test[] t = new Test[6];
-
-        return 6.7;
+        
       
     }
 
